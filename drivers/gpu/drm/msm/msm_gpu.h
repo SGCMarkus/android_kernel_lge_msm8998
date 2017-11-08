4c9dca391f4d9d7b5761dcf6e29687264dec976b
Felix Xiong <xayang@codeaurora.org>
2017-11-08 16:43:44 +0800

    drm/msm: Register GPU devfreq to allow thermal mitigation
    To allow thermal framework to scale the GPU frequency to mitigate
    the GPU temperature. Register GPU as a devfreq based cooling device.
    
    Change-Id: I01176ef96a69f1aa90d30f14f5c9d940e97cc188
    Signed-off-by: Felix Xiong <xayang@codeaurora.org>
    Signed-off-by: Venkateswara Rao Tadikonda <vtadik@codeaurora.org>
    Signed-off-by: Sharat Masetty <smasetty@codeaurora.org>

--- drivers/gpu/drm/msm/msm_gpu.h
+++ drivers/gpu/drm/msm/msm_gpu.h
@@ -147,6 +147,13 @@ struct msm_gpu {
 	struct timer_list hangcheck_timer;
 	struct work_struct recover_work;
 	struct msm_snapshot *snapshot;
+
+	struct {
+		struct devfreq *devfreq;
+		u64 busy_cycles;
+		ktime_t time;
+		struct thermal_cooling_device *cooling_dev;
+	} devfreq;
 };
 
 struct msm_gpu_submitqueue {
